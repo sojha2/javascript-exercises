9d641a4b5278ed496313c239f4e3a3bb29dced84
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-19 16:00:42 -0700

    Fix sumAll so it passes all test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (typeof(num1) != "number" || typeof(num2) != "number") {
+        return "ERROR";
+    }
+
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (num1 < num2) {
+        for(let i = 0; num2 - i >= num1; i++) {
+            sum += num2 - i;
+        }
+    }
+    else {
+        for(let i = 0; num1 - i >= num2; i++) {
+            sum += num1 - i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
