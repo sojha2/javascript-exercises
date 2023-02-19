1bef726d289e4dcbc6c5694ff200b8968fc3abe3
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-19 15:30:21 -0700

    Fix removeFromArray so it passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToBeRemoved) {
 
+    for (let i = 0; i < itemsToBeRemoved.length; i++) {
+        if (array.includes(itemsToBeRemoved[i])) {
+            array.splice(array.indexOf(itemsToBeRemoved[i]), 1);
+        }
+        continue;
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
