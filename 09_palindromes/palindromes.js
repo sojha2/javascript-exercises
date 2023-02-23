fde6dcd29c80cf467f03149c2e262545609882a7
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-22 18:35:24 -0700

    Finish palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    let lowerCase = string.toLowerCase();
+    let reversedString = '';
+    const regEx = /[^a-z]/g;
 
+    lowerCase = lowerCase.replaceAll(regEx, '');
+
+    for(let i = 0; i < lowerCase.length; i++) {
+        reversedString += lowerCase[lowerCase.length - i - 1];
+    }
+
+    const parsedReversedString = reversedString.replaceAll(regEx, '');
+
+    if (parsedReversedString == lowerCase) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
