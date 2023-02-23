bf327d68b85c239f6a00428f1b2b1e9dd76244d0
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-22 19:03:23 -0700

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    fibNMinus1 = 1;
+    fibNMinus2 = 1;
+    let fibN = 1;
 
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+
+    for (let i = 3; i <= n; i++) {
+        fibN = fibNMinus1 + fibNMinus2;
+
+        fibNMinus2 = fibNMinus1;
+        fibNMinus1 = fibN; 
+    }
+    return fibN;
 };
 
 // Do not edit below this line
