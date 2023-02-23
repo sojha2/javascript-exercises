a9b03ee2e2a9d9d5eea47617e86ab1ae22e10823
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-23 08:22:06 -0700

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,53 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleToBeChecked) {
+    let index = 0; 
 
+    for(let i = 0; i < peopleToBeChecked.length; i++) {
+        let currentPersonBirth =  peopleToBeChecked[i].yearOfBirth;
+        let currentPersonDeath = peopleToBeChecked[i].yearOfDeath;
+
+        if (!peopleToBeChecked[i].hasOwnProperty('yearOfDeath')) {
+            currentPersonDeath = new Date();
+            currentPersonDeath = currentPersonDeath.getFullYear();
+        }
+
+        let indexedPersonBirth = peopleToBeChecked[index].yearOfBirth;
+        let indexedPersonDeath = peopleToBeChecked[index].yearOfDeath;
+
+        if (!peopleToBeChecked[index].hasOwnProperty('yearOfDeath')) {
+            indexedPersonDeath = new Date();
+            indexedPersonDeath = indexedPersonDeath.getFullYear();
+        }
+        
+
+        let currentPersonAge = currentPersonDeath - currentPersonBirth;
+        let indexedPersonAge = indexedPersonDeath - indexedPersonBirth;
+
+        if (currentPersonAge > indexedPersonAge) {
+            index = i;
+        } 
+    }
+    
+    return peopleToBeChecked[index];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
